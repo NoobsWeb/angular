7a3d5321ed921c5e0296579230db7578cc8bca58
chen.wang@baifendian.com <chen.wang@baifendian.com>
2016-03-18 17:01:00 +0800

    ''

--- jy/static/js/app.js
+++ jy/static/js/app.js
@@ -1,6 +1,7 @@
 var appModule=angular.module('app', ['ngRoute','ng-pagination']).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
-      when('/alldata', {templateUrl: 'views/manage_job.html',controller:'manage-table'}).     
-      otherwise({redirectTo: '/alldata'});
-}]);
+      when('/manage', {templateUrl: 'views/manage_job.html',controller:'manage-table'}).
+      when('/manage_userImg/:id/:work', {templateUrl: 'views/manage_userImg.html',controller:'manage-userImg'}).     
+      otherwise({redirectTo: '/manage'})
+}])
--- jy/static/js/controller.js
+++ jy/static/js/controller.js
@@ -1,18 +1,35 @@
-appModule.controller('manage-table', function($scope, $http) {
-   $http({method:'get',url:"/jy/static/manage_company.json"}).success(function(data){
-        $scope.companys = data.company;
-        $scope.part = data.part;
-    });
-   	$http({method:'get',url:"/jy/static/table.json"}).success(function(data){
-        $scope.items = data
-    });
-     $scope.onPageChange = function() {
-      // ajax request to load data
-     	$http({method:'get',url:"/jy/static/table_"+$scope.currentPage+".json"}).success(function(data){
-	        $scope.items = data
-    	});
-    };
+/* 岗位管理首页 */
 
-    // set pagecount in $scope
-    $scope.pageCount = 100;
+appModule.controller('manage-table', function($scope, $http) {
+
+  //来源公司与部门数据
+  $http({method:'get',url:"/jy/static/manage_company.json"}).success(function(data){
+    $scope.companys = data.company;
+    $scope.part = data.part;
+  });
+
+  //表格数据
+ 	$http({method:'get',url:"/jy/static/table_1.json"}).success(function(data){
+    $scope.items = data
+  });
+
+  //分页
+  $scope.onPageChange = function() {
+    // ajax request to load data
+   	$http({method:'get',url:"/jy/static/table_"+$scope.currentPage+".json"}).success(function(data){
+      $scope.items = data
+  	});
+  };
+
+  // set pagecount in $scope
+  $scope.pageCount = 100;
+})
+
+/* 岗位管理首页end  */
+
+appModule.controller('manage-userImg', function($scope, $http, $routeParams) {
+  //接收传过来的id和工作 
+  $scope.pid = $routeParams.id;
+  $scope.work = $routeParams.work;
+  console.log($scope.pid)
 })
