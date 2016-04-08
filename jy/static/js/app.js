ae52a584383779454d5c755d1aede3bc1a17b91d
chen.wang@baifendian.com <chen.wang@baifendian.com>
2016-04-08 09:53:40 +0800

    ''

--- jy/static/js/app.js
+++ jy/static/js/app.js
@@ -3,8 +3,10 @@ var appModule=angular.module('app', ['ngRoute','ng-pagination']).
   $routeProvider.
       when('/manage', {templateUrl: 'views/manage_job.html',controller:'manage-table'}).
       when('/manage_userImg/:id&:work&:stTime&:upTime', {templateUrl: 'views/manage_userImg.html',controller:'manage-userImg'}).     
+      when('/manage_userDetail/:id&:work&:stTime&:upTime', {templateUrl: 'views/manage_userDetail.html',controller:'manage-userDetail'}).     
       when('/manage_jdInfo/:id&:work&:stTime&:upTime', {templateUrl: 'views/manage_jdInfo.html',controller:'manage-jdInfo'}).     
       when('/manage_view/:id&:work&:stTime&:upTime', {templateUrl: 'views/manage_view.html',controller:'manage-view'}).     
       when('/manage_talent/:id&:work&:stTime&:upTime&:part&:count&:company', {templateUrl: 'views/manage_talent.html',controller:'manage-talent'}).     
+      when('/match_userImg/:id', {templateUrl: 'views/match_userImg.html',controller:'match-userImg'}).     
       otherwise({redirectTo: '/manage'})
 }])
--- jy/static/js/controller.js
+++ jy/static/js/controller.js
@@ -54,6 +54,41 @@ appModule.controller('manage-userImg', function($scope, $http, $routeParams) {
   })
 })
 
+/* 用户详情 */
+appModule.controller('manage-userDetail',function($scope,$http,$routeParams){
+  $scope.pid = $routeParams.id;
+  $scope.work = $routeParams.work;
+  $scope.enterTime = $routeParams.stTime;
+  $scope.updateTime = $routeParams.upTime;
+  $http({method:'get',url:'/jy/static/detail.json'}).success(function(data){
+    $scope.items = data;
+  })
+  $scope.showList = function(item,event){
+    
+    if ($(event.target).hasClass('fa-plus-square')){
+      $(event.target).parent().siblings('ul').show();
+      $(event.target).removeClass('fa-plus-square').addClass('fa-minus-square')
+    }else if ($(event.target).hasClass('fa-minus-square')){
+      $(event.target).parent().siblings('ul').hide();
+      $(event.target).removeClass('fa-minus-square').addClass('fa-plus-square')
+    }
+    var pFont,liHeight;
+    if($(event.target).next().length !== 0){
+      var len= $(event.target).parents('li').length;
+      for(var i=0;i<len;i++){
+        if($(event.target).parents('li').eq(i).next().length===0){
+          liHeight= $(event.target).parents("li").eq(i).children('ul')[0].offsetHeight;
+          console.log(liHeight);
+          pFont=$(event.target).parents("li").eq(i).children('p');
+          pFont.css({
+            height:liHeight
+          });
+        }
+      }
+    }
+  }
+})
+
 /* JD信息 */
 appModule.controller('manage-jdInfo',function($scope,$http,$routeParams){
   $scope.pid = $routeParams.id;
@@ -65,6 +100,7 @@ appModule.controller('manage-jdInfo',function($scope,$http,$routeParams){
     $scope.items = data;
     data.length = data.length-1;
   })
+
 })
 
 /* 决策洞察 */
@@ -147,13 +183,12 @@ appModule.controller('manage-talent',function($scope,$http,$routeParams){
   $scope.open_ul = function(target){
     event.stopPropagation()
     var btn_open = $(target.target);
-    var status = btn_open.text();
-    if(status == "+"){
+    if(btn_open.hasClass('fa-plus-square')){
       btn_open.parent().siblings('ul').addClass('open')
-      btn_open.text("-");
+      btn_open.removeClass('fa-plus-square').addClass('fa-minus-square');
     }else{
       btn_open.parent().siblings('ul').removeClass('open')
-       btn_open.text("+")
+      btn_open.removeClass('fa-minus-square').addClass('fa-plus-square');
     }
   }
   $scope.remove = function(item,event){
@@ -179,31 +214,53 @@ appModule.controller('manage-talent',function($scope,$http,$routeParams){
       $(event.target).addClass('checked')
       $(event.target).parent().siblings('ul').find('.ul-check').addClass('checked')
     }else{
-      var this_content = this.content.content;
-      if (typeof(this_content) === "string"){
-        data_item.content_right = this_content
-        data_item.content_left = this.$parent.content.title;
-        $scope.checkedItem.push(data_item)
+      if(!this.content){
+        console.log(1)
       }else{
-        console.log(this_content)
-        for (var i=0;i<this_content.length;i++){
-          data_item.content_right = this_content[i].content;
-          data_item.content_left = this.content.title;
+        var item_data = this.content
+        if (typeof(item_data.content) === "string"){
+          data_item.content_right = item_data.content
+          data_item.content_left = this.$parent.content.title;
           $scope.checkedItem.push(data_item)
-          data_item={}
+        }else{
+          (function (item_data){
+            if (item_data.level === "third"){
+              for (var i=0;i<item_data.content.length;i++){
+                data_item.content_right = item_data.content[i].content;
+                data_item.content_left = item_data.title;
+                $scope.checkedItem.push(data_item)
+                data_item={}
+              }
+            }else{
+              for(var i=0;i<item_data.content.length;i++){
+                arguments.callee(item_data.content[i])
+              }
+            }
+          })(item_data)
         }
-      }
+      }    
       $(event.target).addClass('checked')
       $(event.target).parent().siblings('ul').find('.ul-check').addClass('checked')
     }
   }
   $scope.addMtachItem = function(item,event){
-    event.stopPropagation();
-    console.log(item.content)
     item.content = item.content.concat($scope.checkedItem)
     $scope.checkedItem = [];
   }
   document.addEventListener('click', function(){
     $('.ul-match-add').hide();
+  })
+})
+
+/* 人才匹配人才画像 */
+appModule.controller('match-userImg', function($scope, $http, $routeParams) {
+  //接收传过来的id和工作 
+  $scope.pid = $routeParams.id;
+  $scope.imgSrc = 'static/images/pic.png';
+  $http({method:'get',url:"/jy/static/match_userImg.json"}).success(function(data){
+    $scope.userInfo = data;
+  });
+  $http({method:'get',url:'/jy/static/matchuserImg.json'}).success(function(data){
+    $scope.items = data;
   })
 })
